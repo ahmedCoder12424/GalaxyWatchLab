496919360ccca5ff30fb9b422f880794902f0a77
praneeth776 <spraneeth536@gmail.com>
2025-10-30 15:24:48 -0700

    Updated LocationActivity.kt to display Longitude, Latitude and Altitude correctly

--- GPSTracker/app/src/main/java/com/example/gpstracker/presentation/LocationActivity.kt
+++ GPSTracker/app/src/main/java/com/example/gpstracker/presentation/LocationActivity.kt
@@ -1,38 +1,36 @@
 package com.example.gpstracker.presentation
-
 import android.Manifest
 import android.content.pm.PackageManager
 import android.location.Location
 import android.os.Bundle
-import android.os.Looper
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.RequiresPermission
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.style.TextAlign
 import androidx.wear.compose.material.MaterialTheme
 import androidx.wear.compose.material.Text
-import com.google.android.gms.location.LocationCallback
-import com.google.android.gms.location.LocationRequest
-import com.google.android.gms.location.LocationResult
+import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
 
 class LocationActivity : ComponentActivity() {
 
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
         setContent {
             if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-                LocationScreen()
+                LocationText()
             } else {
                 requestPermissions(
                     arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
@@ -53,57 +51,26 @@ class LocationActivity : ComponentActivity() {
         return packageManager.hasSystemFeature(PackageManager.FEATURE_LOCATION_GPS)
     }
 
-    @RequiresPermission(Manifest.permission.ACCESS_FINE_LOCATION)
+    @RequiresPermission(allOf = [Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION])
     @Composable
-    fun LocationScreen() {
+    fun LocationText() {
         if (hasGps()) {
-            var location by remember { mutableStateOf<Location?>(null) }
-            val context = LocalContext.current
-            val fusedLocationClient = remember { LocationServices.getFusedLocationProviderClient(context) }
-
-            val locationCallback = remember {
-                object : LocationCallback() {
-                    override fun onLocationResult(locationResult: LocationResult) {
-                        for (loc in locationResult.locations) {
-                            location = loc
-                        }
+            var locationText by remember { mutableStateOf("...") }
+            fusedLocationClient.lastLocation
+                .addOnSuccessListener { location: Location? ->
+                    if (location != null) {
+                        locationText = "Lat: ${location.latitude}, Lon: ${location.longitude}"
+                    } else {
+                        locationText = "not available"
                     }
                 }
-            }
-
-            DisposableEffect(key1 = fusedLocationClient) {
-                val locationRequest = LocationRequest.create().apply {
-                    interval = 1000
-                    fastestInterval = 500
-                    priority = LocationRequest.PRIORITY_HIGH_ACCURACY
-                }
-
-                fusedLocationClient.requestLocationUpdates(
-                    locationRequest,
-                    locationCallback,
-                    Looper.getMainLooper()
-                )
-
-                onDispose {
-                    fusedLocationClient.removeLocationUpdates(locationCallback)
-                }
-            }
-
-            val locationText = if (location != null) {
-                "Lat: ${location?.latitude}
-Lon: ${location?.longitude}
-Alt: ${location?.altitude}"
-            } else {
-                "Fetching location..."
-            }
 
             Text(
                 modifier = Modifier.fillMaxWidth(),
                 textAlign = TextAlign.Center,
                 color = MaterialTheme.colors.primary,
-                text = locationText
+                text = "Location is $locationText"
             )
-
         } else {
             Text(
                 modifier = Modifier.fillMaxWidth(),
